5ca9b71ce2250e3610e95c694038185d2462a044
Tejaswini <tejaswinithota83@gmail.com~>
2022-12-26 16:03:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,flash,render_template, request  
+import tensorflow as tf
+import numpy as np
+import json
+import requests
+app = Flask(__name__) 
+@app.route('/')
+def index_view():
+    return render_template('home.html')  
+
+
+def pre_process(img):
+    img = tf.keras.preprocessing.image.load_img(img, target_size=(96, 96))
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    return img 
+def prediction():
+    img='image path'
+    img=pre_process(img)
+    url='https://us-central1-global-snow-372118.cloudfunctions.net/crapguru_func_v1'
+    payload = json.dumps({"instances": img.tolist()})
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=payload, headers=headers)
+    result = response.json()
+    predictions = result["predictions"]
+    print(predictions) 
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000) 
