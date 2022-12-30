2af76233a422d11e5330f5ff56ff23d66e92231b
Jesher Joshua <jesher.joshua@gmail.com>
2022-12-30 14:14:05 +0530

    Change message for non-bio

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, jsonify
 import requests
 import tensorflow as tf
@@ -6,7 +7,8 @@ import json
 
 app = Flask(__name__)
 
-UPLOADS = './static/uploads'
+UPLOADS = "./static/uploads"
+
 
 def pre_process(img):
     img = tf.keras.preprocessing.image.load_img(img, target_size=(96, 96))
@@ -15,36 +17,38 @@ def pre_process(img):
     return img
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
 
 @app.route("/resultpage", methods=["POST", "GET"])
 def result():
-    if(request.method=="POST"):
-        img = request.files['filename']
-        img.save(UPLOADS+'test.jpg')
-        img = pre_process(UPLOADS+'test.jpg')
-        url='https://us-central1-global-snow-372118.cloudfunctions.net/crapguru_func_v1'
+    if request.method == "POST":
+        img = request.files["filename"]
+        img.save(UPLOADS + "test.jpg")
+        img = pre_process(UPLOADS + "test.jpg")
+        url = (
+            "https://us-central1-global-snow-372118.cloudfunctions.net/crapguru_func_v1"
+        )
         payload = json.dumps({"instances": img.tolist()})
-        headers = {'Content-type': 'application/json'}
+        headers = {"Content-type": "application/json"}
         response = requests.post(url, data=payload, headers=headers)
         result = response.json()
         pred = result["predictions"]
         if pred == 0:
-            txt ="Your Waste is Bio-Degradable!"
+            txt = "Your Waste is Bio-Degradable!"
         else:
-            txt ="Your Waste is Non-Biodegradable"
+            txt = "Your Waste is Treatable, separate from other wet waste"
         print(txt)
-        #return render_template('res.html',data = txt)
-        return jsonify({"pred":pred,"text":txt})
+        # return render_template('res.html',data = txt)
+        return jsonify({"pred": pred, "text": txt})
 
 
-@app.route('/info')
+@app.route("/info")
 def info():
     return render_template("info.html")
 
 
 if __name__ == "__main__":
-	app.run(debug=True,host="0.0.0.0",port="8000")
+    app.run(debug=True, host="0.0.0.0", port=8080)
